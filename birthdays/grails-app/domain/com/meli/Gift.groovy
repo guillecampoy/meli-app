8e2969d018cef3992c04b344517da27df369cd2f
jbordes <juanpablo.bordes@gmail.com>
2015-04-28 17:56:59 -0300

    Se agrega el belongs to a la clase Gift

--- birthdays/grails-app/domain/com/meli/Gift.groovy
+++ birthdays/grails-app/domain/com/meli/Gift.groovy
@@ -4,7 +4,7 @@ class Gift {
 
 	Integer year
 	String idItem
-
+	static belongsTo = [employee:Employee]
     static constraints = {
     	year nullable: false
     	idItem nullable: false, unique: true
