053a268a1286220efd90603648ab7e972c72f396
jbordes <juanpablo.bordes@gmail.com>
2015-04-28 11:07:06 -0300

    Configuracion de la base #1

--- birthdays/grails-app/conf/DataSource.groovy
+++ birthdays/grails-app/conf/DataSource.groovy
@@ -1,10 +1,18 @@
-dataSource {
+/*dataSource {
     pooled = true
     jmxExport = true
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
+}*/
+
+dataSource {
+  pooled = true
+  jmxExport = true
+  driverClassName = "com.mysql.jdbc.Driver"
+  dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -18,14 +26,15 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
+          dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+          url = "jdbc:mysql://localhost/birthday_dev?useUnicode=yes&characteEncoding=UTF-8"
+          username = "root"
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+          dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+          url = "jdbc:mysql://localhost/birthday_dev?useUnicode=yes&characteEncoding=UTF-8"
+          username = "root"
         }
     }
     production {
