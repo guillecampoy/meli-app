a5fe25f6ccca80f17a447a02c86a0bf29dc2e974
Walter Olivero <walterolivero@MacBook-Pro-de-Walter.local>
2015-04-30 17:22:21 -0300

    Primer version front end

---
+++ birthdays/grails-app/assets/javascripts/meliapp.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+	$("#search-text").keydown(search);
+
+	// Codigo para el dialogo modal
+
+	$(function(){
+
+		var appendthis =  ("<div class='modal-overlay js-modal-close'></div>");
+
+		  $('button[data-modal-id]').click(function(e) {
+		    e.preventDefault();
+		    $("body").append(appendthis);
+		    $(".modal-overlay").fadeTo(500, 0.7);
+		    $(".js-modalbox").fadeIn(500);
+		    var modalBox = $(this).attr('data-modal-id');
+		    $('#'+modalBox).fadeIn($(this).data());
+
+		    //change tittle
+		    $("#dialog-text").text("Buscar regalo para " + this.id);
+		    $()
+
+		  });  
+		  
+		  
+		$(".js-modal-close, .modal-overlay").click(
+			function() {
+				$(".modal-box, .modal-overlay").fadeOut(500, function() {
+					$(".modal-overlay").remove();
+				});
+			}
+		);
+		 
+		$(window).resize(function() {
+		  $(".modal-box").css({
+		    top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+		    left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+		  });
+		});
+		 
+		$(window).resize();
+		 
+		});
+	});
+
+vacio = false;
+
+current_id = "";
+
+function search(){
+	// var url_meli = "https://api.mercadolibre.com/sites/MLA/search?q=" + this.value;
+	// var promise = $.get(url_meli);
+	// promise.done(processResult);
+	_this = this;
+	setTimeout(function() {
+		if (!_this.value == "" & vacio){
+			vacio = !vacio;
+			animateResult();
+		} else{ 
+				if (_this.value == "" & !vacio){
+					vacio = !vacio;
+					animateResult();
+				}
+		}
+		
+		var url_meli = "https://api.mercadolibre.com/sites/MLA/search?q=" + _this.value;
+		var promise = $.get(url_meli);
+		promise.done(processResult);
+
+		}, 0);
+};
+
+function animateResult(){
+	$("#result-conteiner").animate({
+			    // opacity: 0.25,
+			    width: [ "toggle", "swing" ],
+				height: [ "toggle", "swing" ],
+				opacity: "toggle"
+			  }, 500, function() {
+			    // cuando termina la animacion
+			  });
+}
+
+function processResult(data){
+	$("#result-conteiner").empty();
+	console.log(data.results.length);
+	data.results.forEach(function(item){
+		var div_item = "<div class='row' id='"+item.id+"'>"+"<img class='item-image' src='"+item.thumbnail+"'/>"+ "<p class='title'>"+item.title+"</p>" +"</div>"
+		$("#result-conteiner").append(div_item);
+	});
+
+	$(".row").bind("click", function(){
+
+				addGiftToEmployee(this);
+
+				$(".modal-box, .modal-overlay").fadeOut(500, function() {
+					$(".modal-overlay").remove();
+				});
+		});
+
+};
+
+function addGiftToEmployee(gift_div){
+
+	console.log("que esss"+gift_div);
+
+	// var div_item = "<div class='row' id='"+item.id+"'>"+"<img class='item-image' src='"+item.thumbnail+"'/>"+ "<p class='title'>"+item.title+"</p>" +"</div>"
+	$("#gift"+current_id).html($(gift_div).html());
+
+};
+
+
+function addGift(_this){
+	// console.log("id button: " + $(_this).attr("id"));
+	current_id = $(_this).attr("id");
+	$( "#dialog" ).dialog({
+      resizable: false,
+      height:140,
+      modal: true,
+      buttons: {
+        "Delete all items": function() {
+          $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+};
+
+function closePopup(){
+
+	//cuando cierra popup
+
+}
